f22677f39d1ea1b87205f48c7fb729cf821b57c5
Yeleti Nandhu reddy <nandu.intern@gmail.com>
2024-11-06 19:39:41 +0530

    first commit

---
+++ src/app/cart/cart.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  private apiCartUrl = environment.apiUrl + "/cart";
+  private apiCheckOutUrl = environment.apiUrl + "/checkout";
+
+  constructor(private http: HttpClient) { }
+
+  addToCart(product: Product): Observable<Product>{
+    return this.http.post<Product>(this.apiCartUrl,product)
+  }
+
+  getCartItems(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.apiCartUrl)
+  }
+
+  clearCart(): Observable<void>{
+    return this.http.delete<void>(this.apiCartUrl)
+  }
+
+  checkout(products:Product[]): Observable<void>{
+return this.http.post<void>(this.apiCheckOutUrl,products)
+  }
+}
